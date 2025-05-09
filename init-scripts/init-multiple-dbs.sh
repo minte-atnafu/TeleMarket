3c03e4f968abb59d185f4ca00add3f20b8d5d9a3
Minte <163767827+minte-atnafu@users.noreply.github.com>
2025-05-10 00:44:45 +0300

    randomize display of products

---
+++ init-scripts/init-multiple-dbs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Split comma-separated database names into array
+IFS=',' read -ra DBS <<< "$POSTGRES_MULTIPLE_DATABASES"
+
+for db in "${DBS[@]}"; do
+  echo "Creating database: $db"
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE "$db";
+EOSQL
+done
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+@login_required
+def profile_view(request):
+    user = request.user
+    context = {
+        'user': user,
+        'date_joined': user.date_joined.strftime("%B %d, %Y"),
+    }
+    return render(request, 'users/profile.html', context)  # Note template path
